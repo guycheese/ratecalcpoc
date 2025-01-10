5d8aa4a16506e84b19c74b88471f1ff1b142feb2
guycheese <surajshyam2002@gmail.com>
2025-01-10 12:09:32 +0000

    Changed the code to use signals.

--- src/app/ratecalculator/ratecalculator.component.ts
+++ src/app/ratecalculator/ratecalculator.component.ts
@@ -1,8 +1,9 @@
-import { Component, input, OnInit } from '@angular/core';
+import { Component, computed, signal, OnInit, model } from '@angular/core';
 import { BaseDataService } from '../services/base-data.service';
 import { FormsModule } from '@angular/forms';
 import { round2DP, roundUp2DP } from '../../helpers';
 import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-ratecalculator',
   imports: [FormsModule, CommonModule],
@@ -24,105 +25,97 @@ export class RatecalculatorComponent implements OnInit {
 
   constructor(private baseDataService: BaseDataService) {}
 
-  payRate = 0;
-  chargeSSP = true;
-  overtime = false;
-  private _margin = 0;
-  private _marginPercentage = 0;
+  payRate = model(0);
+  chargeSSP = model(true);
+  overtime = model(false);
+  margin = model(0);
+  marginPercentage = model(0);
 
-  get margin(): number {
-    return this._margin;
+  updateMargin(value: number) {
+    this.margin.set(round2DP(value));
+    const newChargeRate = this.costOfEmployment() + value;
+    this.marginPercentage.set(round2DP((value / newChargeRate) * 100));
   }
 
-  set margin(value: number) {
-    this._margin = value;
-    const newChargeRate = this.costOfEmployment + value;
-    this._marginPercentage = round2DP((value / newChargeRate) * 100);
+  updateMarginPercentage(value: number) {
+    this.marginPercentage.set(value);
+    this.margin.set(round2DP(this.chargeRate() * (value / 100)));
   }
 
-  get marginPercentage(): number {
-    return this._marginPercentage;
-  }
-
-  set marginPercentage(value: number) {
-    this._marginPercentage = value;
-    this._margin = round2DP(this.chargeRate * (value / 100));
-  }
-
-  get holidayPay(): number {
+  holidayPay = computed(() => {
     return Math.max(
       0,
       round2DP(
         (this.baseData.holidayPerAnum / (260 - this.baseData.holidayPerAnum)) *
-          this.payRate
+          this.payRate()
       )
     );
-  }
+  });
 
-  get nationalInsurance(): number {
-    const niThreshold = this.overtime ? 0 : this.baseData.nicThreshold;
+  nationalInsurance = computed(() => {
+    const niThreshold = this.overtime() ? 0 : this.baseData.nicThreshold;
     return Math.max(
       0,
       round2DP(
-        (((this.holidayPay + this.payRate) * this.baseData.workingHours -
+        (((this.holidayPay() + this.payRate()) * this.baseData.workingHours -
           niThreshold) *
           (this.baseData.niPercentage / 100)) /
           this.baseData.workingHours
       )
     );
-  }
+  });
 
-  get pension(): number {
+  pension = computed(() => {
     return Math.max(
       0,
       round2DP(
-        (this.payRate + this.nationalInsurance) *
+        (this.payRate() + this.nationalInsurance()) *
           (this.baseData.pensionPercentage / 100)
       )
     );
-  }
+  });
 
-  get apprenticeshipLevy(): number {
+  apprenticeshipLevy = computed(() => {
     return Math.max(
       0,
       roundUp2DP(
-        (this.payRate + this.nationalInsurance) *
+        (this.payRate() + this.nationalInsurance()) *
           (this.baseData.appLevyPercentage / 100)
       )
     );
-  }
+  });
 
-  get ssp(): number {
-    if (this.chargeSSP && this.baseData.sspPercentage) {
-      return round2DP((this.baseData.sspPercentage / 100) * this.payRate);
+  ssp = computed(() => {
+    if (this.chargeSSP() && this.baseData.sspPercentage) {
+      return round2DP((this.baseData.sspPercentage / 100) * this.payRate());
     }
     return 0;
-  }
+  });
 
-  get costOfEmployment(): number {
+  costOfEmployment = computed(() => {
     return round2DP(
-      this.payRate +
-        this.holidayPay +
-        this.nationalInsurance +
-        this.pension +
-        this.apprenticeshipLevy +
-        this.ssp
+      this.payRate() +
+        this.holidayPay() +
+        this.nationalInsurance() +
+        this.pension() +
+        this.apprenticeshipLevy() +
+        this.ssp()
     );
-  }
+  });
 
-  get chargeRate(): number {
-    return round2DP(this.costOfEmployment / (1 - this.marginPercentage / 100));
-  }
-
-  ///////
+  chargeRate = computed(() => {
+    return round2DP(
+      this.costOfEmployment() / (1 - this.marginPercentage() / 100)
+    );
+  });
 
   ngOnInit(): void {
     this.baseData = this.baseDataService.getBaseData();
   }
 
   toggleSSP() {
-    if (this.chargeSSP == true && this.overtime == true) {
-      this.chargeSSP = false;
+    if (this.chargeSSP() && this.overtime()) {
+      this.chargeSSP.set(false);
     }
   }
 }
